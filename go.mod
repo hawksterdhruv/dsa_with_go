a291df8355f21703631adaf7c233ef69367a216f
Dhruv <dhruv@Dhruvs-MacBook-Pro.local>
2021-10-09 17:05:44 +0530

    spiltting linkedlist package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hawksterdhruv/dsa_with_go
+
+go 1.17
--- main.go
+++ main.go
@@ -1,51 +1,8 @@
 package main
 
-import "fmt"
-
-func printHello() {
-	fmt.Println("Hello World")
-}
-
-type Node struct {
-	data int
-	next *Node
-}
-
-type LinkedList struct {
-	root *Node
-}
-
-func (ll *LinkedList) addNode(data int) {
-	node := Node{data: data}
-	if ll.root == nil {
-		ll.root = &node
-	} else {
-		current := ll.root
-		for current.next != nil {
-			current = current.next
-		}
-		current.next = &node
-	}
-}
-
-func (ll *LinkedList) addNodeAtBeginning(data int) {
-	node := Node{data: data}
-	if ll.root == nil {
-		ll.root = &node
-	} else {
-		node.next = ll.root
-		ll.root = &node
-	}
-}
-
-func (ll LinkedList) display() {
-	current := ll.root
-	fmt.Println()
-	for current != nil {
-		fmt.Printf("%v ->", current.data)
-		current = current.next
-	}
-}
+import (
+	"fmt"
+)
 
 func main() {
 	var linkedList LinkedList
